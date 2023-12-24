a0cc1b838651cbca9cdb3aeaf1807c5531e8b57a
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-12-24 09:40:37 -0300

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -37,6 +38,7 @@ export default function RootLayout({
               {children}
             </Container>
             <SpeedInsights />
+            <Analytics />
           </Container>
         </Providers>
       </body>
