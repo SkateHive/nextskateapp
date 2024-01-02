164e9f3c819ab0aec2dc01c0009116bf99e59e5b
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-01-02 14:51:30 -0300

    refactor: remove unused code

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -6,35 +6,11 @@ import { useEffect } from "react"
 import Post from "../Post"
 
 export default function Feed() {
-  // const [posts, setPosts] = useState<PostComponentProps[]>(
-  //   [] as PostComponentProps[]
-  // )
-  // const [isLoading, setIsLoading] = useState(false)
-
   const { posts, getPosts, isLoadingPosts } = usePostsContext()
 
   useEffect(() => {
-    console.log(posts.length)
     if (posts.length == 0) getPosts(10)
   }, [])
-
-  // const handleClickLoadMore = async (threshold: number = 0) => {
-  // loadMorePosts(threshold)
-  //   getPosts(threshold)
-  // }
-
-  // const loadMorePosts = async (threshold: number = 0) => {
-  //   try {
-  //     setIsLoading(true)
-  //     const res = await fetch(`/api/posts?limit=${threshold + 10}`)
-  //     const data = await res.json()
-  //     setPosts(data)
-  //   } catch (err: any) {
-  //     console.log(err)
-  //   } finally {
-  //     setIsLoading(false)
-  //   }
-  // }
 
   return (
     <VStack align="stretch" spacing={[2, 4]} p={2}>
