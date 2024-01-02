50da18cfcbeb6300644960b2832cdfea1ce23ba5
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-01-02 11:04:27 -0300

    fix: feed cache revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
 import FeedPage from "@/components/FeedPage"
+import { PostComponentProps } from "@/components/Post"
+import HiveClient from "@/lib/hiveclient"
+import PostModel from "@/lib/models/post"
 
-export default function Home() {
-  return <FeedPage />
+export const revalidate = 30
+
+const SKATEHIVE_TAG = "hive-173115"
+const hiveClient = HiveClient()
+
+async function getData(threshold: number = 0): Promise<PostComponentProps[]> {
+  const response = await hiveClient.database.getDiscussions("created", {
+    tag: SKATEHIVE_TAG,
+    limit: threshold + 10,
+  })
+  if (Array.isArray(response) && response.length > 0)
+    return await Promise.all(
+      response.map(async (postData) => {
+        const post = PostModel.newFromDiscussion(postData)
+        const user = await post.authorDetails()
+        return { postData: post.simplify(), userData: user.simplify() }
+      })
+    )
+  return [{} as PostComponentProps]
+}
+
+export default async function Home() {
+  const data = await getData()
+  return <FeedPage posts={data} />
 }
--- src/components/FeedPage.tsx
+++ src/components/FeedPage.tsx
@@ -1,35 +1,12 @@
-import HiveClient from "@/lib/hiveclient"
-import PostModel from "@/lib/models/post"
 import { VStack } from "@chakra-ui/react"
 import Post, { PostComponentProps } from "./Post"
-const SKATEHIVE_TAG = "hive-173115"
 
-const hiveClient = HiveClient()
-
-async function getData(threshold: number = 0): Promise<PostComponentProps[]> {
-  const response = await hiveClient.database.getDiscussions("created", {
-    tag: SKATEHIVE_TAG,
-    limit: threshold + 10,
-  })
-  if (Array.isArray(response) && response.length > 0)
-    return await Promise.all(
-      response.map(async (postData) => {
-        const post = PostModel.newFromDiscussion(postData)
-        const user = await post.authorDetails()
-        return { postData: post.simplify(), userData: user.simplify() }
-      })
-    )
-  return [{} as PostComponentProps]
-}
-
-export default async function FeedPage() {
-  const data = await getData()
+export default function FeedPage({ posts }: { posts: PostComponentProps[] }) {
   return (
     <VStack align="stretch" spacing={[2, 4]} p={2}>
-      {data &&
-        data.map(({ postData, userData }, i) => (
-          <Post key={i} postData={postData} userData={userData} />
-        ))}
+      {posts.map(({ postData, userData }, i) => (
+        <Post key={i} postData={postData} userData={userData} />
+      ))}
     </VStack>
   )
 }
