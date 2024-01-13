156ef9a073578fbfbb3d1ec90c378c7d035d78a9
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-01-13 10:31:03 -0300

    feat: post skeleton

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -2,29 +2,34 @@
 
 import { usePosts } from "@/hooks/usePosts"
 import PostModel from "@/lib/models/post"
-import { Flex, VStack } from "@chakra-ui/react"
+import { Box, Flex, VStack } from "@chakra-ui/react"
 import { useState } from "react"
 import InfiniteScroll from "react-infinite-scroll-component"
 import { BeatLoader } from "react-spinners"
 import Post from "../Post"
+import PostSkeleton from "../Post/Skeleton"
 
 export default function Feed() {
-  const { posts, error } = usePosts()
-  const [visiblePosts, setVisiblePosts] = useState(5)
+  const { posts, error, isLoading } = usePosts()
+  const [visiblePosts, setVisiblePosts] = useState(3)
 
   if (error) return "Error"
-  if (!posts)
+
+  if (isLoading || !posts)
     return (
-      <Flex w={"100%"} justify={"center"}>
-        <BeatLoader size={8} color="darkgrey" />
-      </Flex>
+      <Box p={1}>
+        <PostSkeleton />
+        <PostSkeleton />
+        <PostSkeleton />
+      </Box>
     )
+
   return (
     <VStack align="stretch" spacing={[2, 4]}>
       <InfiniteScroll
         style={{ padding: 2 }}
         dataLength={visiblePosts}
-        next={() => setVisiblePosts((visiblePosts) => visiblePosts + 5)}
+        next={() => setVisiblePosts((visiblePosts) => visiblePosts + 3)}
         hasMore={visiblePosts !== 100}
         loader={
           <Flex w={"100%"} justify={"center"}>
---
+++ src/components/Post/Skeleton.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Card,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Skeleton,
+  SkeletonCircle,
+  Stack,
+} from "@chakra-ui/react"
+
+export default function PostSkeleton() {
+  return (
+    <Card
+      size="sm"
+      boxShadow="none"
+      borderRadius="lg"
+      _hover={{
+        outline: "1px solid",
+        outlineColor: "gray.100",
+      }}
+      mt={2}
+    >
+      <CardHeader pb={0}>
+        <Flex gap="4" align={"end"}>
+          <Flex flex="1" gap="2" alignItems="center">
+            <SkeletonCircle fadeDuration={4} size="10" />
+            <Flex flexDir="column" gap={1}>
+              <Flex gap={1} alignItems="center">
+                <Skeleton h={4} w={32} />
+              </Flex>
+              <Skeleton h={4} w={64} />
+            </Flex>
+          </Flex>
+          <Flex gap={1} align={"center"}>
+            <Skeleton h={4} w={16} />
+          </Flex>
+        </Flex>
+      </CardHeader>
+      <Box p={3}>
+        <Skeleton
+          border={"1px"}
+          borderColor={"gray.50"}
+          w="100%"
+          aspectRatio={16 / 9}
+          borderRadius="md"
+        />
+      </Box>
+      <CardFooter pt={0} flexDirection={"column"} gap={2}>
+        <Flex w={"100%"} justify={"space-between"} align={"center"}>
+          <Skeleton h={4} w={48} />
+          <Stack direction={"row"} gap={1}>
+            <SkeletonCircle fadeDuration={4} size="5" />
+            <SkeletonCircle fadeDuration={4} size="5" />
+            <SkeletonCircle fadeDuration={4} size="5" />
+          </Stack>
+        </Flex>
+      </CardFooter>
+    </Card>
+  )
+}
