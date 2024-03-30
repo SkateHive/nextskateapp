28f769d76bd07d27456a7c1b237938ae6c767ab5
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-30 11:05:04 -0300

    style(post-vote): add char conffeti to post likes

--- src/components/Post/Vote.tsx
+++ src/components/Post/Vote.tsx
@@ -2,8 +2,9 @@ import { usePostContext } from "@/contexts/PostContext"
 import { useHiveUser } from "@/contexts/UserContext"
 import { SWR_POSTS_TAG } from "@/hooks/usePosts"
 import { vote } from "@/lib/hive/functions"
-import { Flex, Text, Tooltip } from "@chakra-ui/react"
+import { Button, Text, Tooltip } from "@chakra-ui/react"
 import { useState } from "react"
+import { useReward } from "react-rewards"
 import { useSWRConfig } from "swr"
 
 export default function Vote() {
@@ -11,12 +12,19 @@ export default function Vote() {
   const { mutate } = useSWRConfig()
   const { hiveUser } = useHiveUser()
 
+  const rewardId = post.post_id ? "postReward" + post.post_id : ""
+  const { reward, isAnimating } = useReward(rewardId, "emoji", {
+    emoji: ["$", "*", "#"],
+    spread: 60,
+  })
+
   const [isVoted, setIsVoted] = useState(
     !!(hiveUser && hiveUser.name && post.userHasVoted(hiveUser.name))
   )
 
   const handleVoteClick = async () => {
     if (hiveUser) {
+      if (!isVoted) reward()
       const voteWeight = isVoted ? 0 : 10000
       await vote({
         username: hiveUser.name,
@@ -31,20 +39,29 @@ export default function Vote() {
 
   return (
     <Tooltip label="Earnings">
-      <Flex
-        gap={1}
-        align={"center"}
-        cursor={hiveUser ? "pointer" : "default"}
+      <Button
+        variant={"link"}
+        disabled={isAnimating}
+        colorScheme={isVoted || isAnimating ? "green" : "white"}
         onClick={hiveUser ? handleVoteClick : () => {}}
       >
+        <span
+          id={rewardId}
+          style={{
+            left: "50%",
+            bottom: "15px",
+            transform: "translateX(-50%)",
+            zIndex: 5,
+          }}
+        />
         <Text
-          color={isVoted ? "limegreen" : "white"}
+          // color={isVoted ? "limegreen" : "white"}
           fontSize={"28px"}
           fontWeight={"bold"}
         >
           ${post.getEarnings().toFixed(2)}
         </Text>
-      </Flex>
+      </Button>
     </Tooltip>
   )
 }
