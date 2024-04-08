d4406dcfed67bbad58940f50f3070fcc423fb095
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-04-07 23:05:05 -0300

    Merge Server Login and Posting with keychain and privateKey works

--- src/app/upload/utils/hiveUpload.tsx
+++ src/app/upload/utils/hiveUpload.tsx
@@ -5,9 +5,11 @@
 import { Client } from "@hiveio/dhive";
 import useAuthHiveUser from "@/lib/useHiveAuth";
 import { useHiveUser } from "@/contexts/UserContext";
+import * as dhive from "@hiveio/dhive"
 
 import CryptoJS from "crypto-js";
-
+import { commentWithPrivateKey } from "@/lib/hive/server-functions";
+import { comment } from "@uiw/react-md-editor";
 const slugify = (text: string) => {
     return text.toString().toLowerCase()
         .replace(/\s+/g, '-')           // Replace spaces with -
@@ -116,64 +118,74 @@ const hiveUpload = async (username: string, title: string, body: string, benefic
             }
         }
     }
-    else {
-        // Assuming 'decryptedPrivateKey' is the user's posting private key
-        const encryptedPrivateKey = localStorage.getItem("postingKey") || "";
-        const secret = process.env.NEXT_PUBLIC_CRYPTO_SECRET || ""
+    else if (localStorage.getItem('LoginMethod') === 'privateKey') {
+        if (user && title) {
+            if (username) {
+                const permlink = slugify(title.toLowerCase());
+                const formatBeneficiaries = (beneficiariesArray: any[]) => {
+                    let seen = new Set();
+                    let finalBeneficiaries = beneficiariesArray.filter(({ account }: { account: string }) => {
+                        const duplicate = seen.has(account);
+                        seen.add(account);
+                        return !duplicate;
+                    }).map(beneficiary => ({
+                        account: beneficiary.account,
+                        weight: parseInt(beneficiary.weight, 10) // Ensure weight is an integer
+                    }));
 
-        const bytes = CryptoJS.AES.decrypt(encryptedPrivateKey, secret) || "";
-        const privateKey2 = bytes.toString(CryptoJS.enc.Utf8);
-        const dhive = require('@hiveio/dhive');
-        // Ensure you have the user's private key decrypted correctly
-        const privateKey = dhive.PrivateKey.fromString(privateKey2);
-        const finalBeneficiaries = beneficiariesArray.map((beneficiary) => ({
-            account: beneficiary.account,
-            weight: parseInt(beneficiary.weight, 10),
-        }));
-        const operations = [
-            // The post operation as previously defined
-            [
-                'comment',
-                {
-                    parent_author: '',
-                    parent_permlink: "safasfasfsa", // Assuming first tag as the main category
+                    // Sort beneficiaries by account name to maintain consistency
+                    finalBeneficiaries = finalBeneficiaries.sort((a, b) => a.account.localeCompare(b.account));
+                    return finalBeneficiaries;
+                };
+
+                // Use this function in your hiveUpload to prepare beneficiaries:
+                let finalBeneficiaries = formatBeneficiaries(beneficiariesArray);
+
+                const commentOptions: dhive.CommentOptionsOperation = ['comment_options', {
                     author: username,
-                    permlink: generatePermlink(title),
-                    title: title,
-                    body: body,
-                    json_metadata: JSON.stringify({
-                        tags: tags,
-                        app: 'skatehive.app',
-                        image: [thumbnail],
-                    }),
-                },
-            ],
-            // Comment options operation
-            [
-                'comment_options',
-                {
-                    author: username,
-                    permlink: generatePermlink(title),
+                    permlink: permlink,
                     max_accepted_payout: '10000.000 HBD',
                     percent_hbd: 10000,
                     allow_votes: true,
                     allow_curation_rewards: true,
-                    extensions: [[0, { beneficiaries: finalBeneficiaries }]],
-                },
-            ],
-        ];
-        const client = new Client('https://api.hive.blog');
-        // Broadcasting the operations
-        client.broadcast.sendOperations(operations as any, privateKey).then(
-            (result: any) => {
-                console.log('Post submitted:', result);
-                // Handle successful submission here (e.g., notifying the user)
-            },
-            (error: any) => {
-                console.error('Failed to submit post:', error);
-                // Handle errors here (e.g., informing the user of the failure)
+                    extensions: [
+                        [0, {
+                            beneficiaries: finalBeneficiaries
+                        }]
+                    ]
+                }];
+                const postOperation: dhive.CommentOperation = [
+                    'comment',
+                    {
+                        parent_author: '',
+                        parent_permlink: generatePermlink(title),
+                        // parent_permlink: process.env.COMMUNITY || 'hive-173115',
+                        author: username,
+                        permlink: permlink,
+                        title: title,
+                        body: body,
+                        json_metadata: JSON.stringify({
+                            tags: tags,
+                            app: 'Skatehive App',
+                            image: thumbnail,
+                        }),
+                    },
+                ];
+
+                console.log("Post Operation:", postOperation);
+
+
+                // Construct the operations array
+                const operations = [postOperation, commentOptions];
+                console.log("Öperations ", operations);
+
+                commentWithPrivateKey(localStorage.getItem("EncPrivateKey"), postOperation, commentOptions);
             }
-        );
+        }
+
+
+
+
     }
 };
 
