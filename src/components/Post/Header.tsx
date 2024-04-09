65fb88632d8c8d38539566a60795ba9b3a92e6d0
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-04-09 07:56:39 -0300

    feat(post-header): add share post menu

--- src/components/Post/Header.tsx
+++ src/components/Post/Header.tsx
@@ -1,18 +1,27 @@
 import { usePostContext } from "@/contexts/PostContext"
 import UserModel from "@/lib/models/user"
+import { getWebsiteURL } from "@/lib/utils"
 import {
+  Button,
   CardHeader,
   Flex,
   HStack,
   Icon,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
   Text,
   Tooltip,
   useDisclosure,
+  useMediaQuery,
 } from "@chakra-ui/react"
-import { Eye } from "lucide-react"
+import { ChevronDownIcon, Copy, ExternalLink, Eye, Twitter } from "lucide-react"
 import moment from "moment-timezone"
 import Link from "next/link"
 import { useEffect, useState } from "react"
+import { FaDiscord } from "react-icons/fa"
 import PostModal from "../PostModal"
 import PostAvatar from "./Avatar"
 
@@ -27,6 +36,76 @@ export default function Header({ variant = "preview" }: HeaderInterface) {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const postAvatar = authorData.metadata?.profile?.profile_image
 
+  const [isSmallerThan400] = useMediaQuery("(max-width: 400px)")
+
+  const postFullUrl = `${getWebsiteURL()}/post${post.url}`
+  const postSummary = `Check out this awesome post on SkateHive by @${post.author} \n\n`
+
+  const handleCopyPostLink = () => {
+    try {
+      const postPageUrl = postFullUrl
+      navigator.clipboard.writeText(postPageUrl)
+    } catch (error) {
+      console.error("Failed to copy the link:", error)
+    }
+  }
+
+  const handleOpenPostLink = () => {
+    try {
+      window.open(postFullUrl, "_blank")
+    } catch (error) {
+      console.error("Failed to open the link:", error)
+    }
+  }
+
+  const handleOpenPostLinkPeakd = () => {
+    try {
+      window.open(`https://peakd.com${post.url}`, "_blank")
+    } catch (error) {
+      console.error("Failed to open the link:", error)
+    }
+  }
+
+  const handleShareWarpCast = async () => {
+    try {
+      const warptext = `${postSummary} ${postFullUrl}`
+      const postPageUrl = encodeURI(warptext)
+      window.open(
+        `https://warpcast.com/~/compose?text=${postPageUrl}`,
+        "_blank"
+      )
+    } catch (error) {
+      console.error("Failed to share in WarpCast:", error)
+    }
+  }
+
+  const handleShareTwitter = async () => {
+    try {
+      const tweetText = `${postSummary} ${postFullUrl}`
+      const postPageUrl = encodeURI(tweetText)
+      window.open(
+        `https://twitter.com/intent/tweet?text=${postPageUrl}`,
+        "_blank"
+      )
+    } catch (error) {
+      console.error("Failed to share in Twitter:", error)
+    }
+  }
+
+  const handleShareDiscord = async () => {
+    try {
+      const postPageUrl = encodeURI(postFullUrl)
+      const discordText = `${postSummary} ${postPageUrl}`
+      navigator.clipboard.writeText(discordText)
+      window.open(
+        "https://discord.com/channels/631777256234156033/631778823716864011",
+        "_blank"
+      )
+    } catch (error) {
+      console.error("Failed to share in Discord:", error)
+    }
+  }
+
   useEffect(() => {
     const fetchAuthor = async () => {
       const author = await UserModel.getNewFromUsername(post.author)
@@ -38,7 +117,11 @@ export default function Header({ variant = "preview" }: HeaderInterface) {
   return (
     <CardHeader p={2} pb={0}>
       <PostModal isOpen={isOpen} onClose={onClose} />
-      <Flex gap="4" align={"start"}>
+      <Flex
+        gap="4"
+        align={"start"}
+        flexDir={variant == "open" && isSmallerThan400 ? "column" : "row"}
+      >
         <Flex flex="1" gap="2" alignItems="center">
           <Link href={post.getFullAuthorUrl()}>
             <PostAvatar
@@ -80,7 +163,59 @@ export default function Header({ variant = "preview" }: HeaderInterface) {
               color="limegreen"
             />
           </Tooltip>
-        ) : null}
+        ) : (
+          <Menu
+            placement={
+              variant == "open" && isSmallerThan400 ? "bottom" : "bottom-end"
+            }
+          >
+            <MenuButton
+              colorScheme="green"
+              as={Button}
+              rightIcon={<ChevronDownIcon />}
+              variant={"outline"}
+              width={variant == "open" && isSmallerThan400 ? "100%" : "auto"}
+            >
+              Share
+            </MenuButton>
+            <MenuList fontSize={"sm"}>
+              <MenuItem onClick={handleCopyPostLink} icon={<Copy size={18} />}>
+                Copy link
+              </MenuItem>
+              <MenuItem
+                onClick={handleShareWarpCast}
+                icon={<ExternalLink size={18} />}
+              >
+                WarpCast
+              </MenuItem>
+              <MenuItem
+                onClick={handleShareTwitter}
+                icon={<Twitter size={18} />}
+              >
+                Twitter
+              </MenuItem>
+              <MenuItem
+                onClick={handleShareDiscord}
+                icon={<FaDiscord size={18} />}
+              >
+                Discord
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem
+                onClick={handleOpenPostLink}
+                icon={<ExternalLink size={18} />}
+              >
+                Post page
+              </MenuItem>
+              <MenuItem
+                onClick={handleOpenPostLinkPeakd}
+                icon={<ExternalLink size={18} />}
+              >
+                PeakD
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        )}
       </Flex>
     </CardHeader>
   )
