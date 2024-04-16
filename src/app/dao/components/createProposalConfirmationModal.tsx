ca908b6e29c16191d4a1f253f4ee77dab4fe9cd9
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-04-16 18:49:17 -0300

    ProPost

--- src/app/dao/components/createProposalConfirmationModal.tsx
+++ src/app/dao/components/createProposalConfirmationModal.tsx
@@ -1,14 +1,14 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useCallback } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import { Web3Provider } from '@ethersproject/providers';
-import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Text } from "@chakra-ui/react";
-import { get, set } from "lodash";
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from "@chakra-ui/react";
 import { MarkdownRenderers } from '@/app/upload/utils/MarkdownRenderers';
 import { createProposal } from '../utils/createProposal';
 import Confetti from 'react-confetti';
-
+import { useHiveUser } from '@/contexts/UserContext';
+import { commentWithKeychain } from '@/lib/hive/client-functions';
 export enum ProposalType {
     SingleChoice = 'single-choice',
     Approval = 'approval',
@@ -16,21 +16,6 @@ export enum ProposalType {
     RankedChoice = 'ranked-choice',
     Weighted = 'weighted',
     Basic = 'basic'
-}
-export interface ProposalData {
-    from?: string;
-    space: string;
-    timestamp?: number;
-    type: ProposalType;
-    title: string;
-    body: string;
-    discussion: string;
-    choices: string[];
-    start: number;
-    end: number;
-    snapshot: number;
-    plugins: string;
-    app?: string;
 }
 
 interface CreateProposalConfirmationModalProps {
@@ -41,52 +26,100 @@ interface CreateProposalConfirmationModalProps {
     title: string;
 }
 
+const generatePermlink = (text: string) => text.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase();
+
 const CreateProposalConfirmationModal: React.FC<CreateProposalConfirmationModalProps> = ({ isOpen, onClose, proposalBody, connectedUserAddress, title }) => {
     const web3 = useMemo(() => new Web3Provider(window.ethereum), []);
-    const [currentBlockNumber, setCurrentBlockNumber] = useState(0);
-    const [confetti, setConfetti] = useState(true);
+    const [state, setState] = useState({ currentBlockNumber: 0, confetti: false });
+    const hiveUser = useHiveUser();
 
-    const fetchCurrentBlockNumber = async () => {
+    const fetchCurrentBlockNumber = useCallback(async () => {
         const blockNumber = await web3.getBlockNumber();
-        console.log('Current block number:', blockNumber);
-        setCurrentBlockNumber(blockNumber);
-    };
+        return blockNumber;  // Return the block number directly
+    }, [web3]);
 
-    const handleCreateProposal = async (web3: Web3Provider, proposalBody: string, connectedUserAddress: string, title: string) => {
-        await fetchCurrentBlockNumber();
-        const currentTimeInSeconds = Math.floor(Date.now() / 1000);
-        const start = currentTimeInSeconds;
-        const end = start + 172800;  // 48 hours converted to seconds
-
-        let ProposalData = {
-            space: "skatehive.eth",
-            type: "single-choice" as ProposalType,
-            title: title,
-            body: proposalBody,
-            discussion: '',
-            choices: ['For', 'Against'],
-            start: start,
-            end: end,
-            snapshot: currentBlockNumber,
-            plugins: JSON.stringify({}),
-            app: 'Skatehive App'
-        };
+    const handleCreateProposal = useCallback(async () => {
         try {
+            const currentBlockNumber = await fetchCurrentBlockNumber();  // Directly use the fetched block number
+
+            if (currentBlockNumber <= 0) {
+                throw new Error('Invalid block number, please try again');
+            }
+            const proPostPermlink = generatePermlink(title);
+            const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+            const start = currentTimeInSeconds;
+            const end = start + 172800; // 48 hours in seconds
+
+            const propostBody = `
+            ---
+            HiveUser: ${hiveUser?.hiveUser?.name}
+            PermLink: ${proPostPermlink}
+            ---
+            ${proposalBody}
+            `
 
 
-            const receipt = await createProposal(web3, ProposalData);
-            console.log('Proposal receipt:', receipt);
-            alert('Proposal submitted successfully!');
+            let proposalData = {
+                space: "skatehive.eth",
+                type: ProposalType.SingleChoice,
+                title: title,
+                body: propostBody,
+                discussion: '',
+                choices: ['For', 'Against'],
+                start: start,
+                end: end,
+                snapshot: currentBlockNumber,
+                plugins: JSON.stringify({}),
+                app: 'Skatehive App'
+            };
+            console.log(proposalData);
+
+            const hivePostMetadata = {
+                "data": {
+                    username: hiveUser?.hiveUser?.name,
+                    title: title,
+                    body: '> ProPost !' + proposalBody,
+                    parent_perm: "blog",
+                    json_metadata: JSON.stringify({
+                        format: "markdown", description: "", tags: ['skatehive', 'skateboarding', 'proposal', 'dao', 'skatehive-proposal'], image: 'https://skatehive.com/images/skatehive-logo.png',
+                    }),
+                    permlink: generatePermlink(title) + "proposal",
+                    comment_options: JSON.stringify({
+                        author: hiveUser?.hiveUser?.name,
+                        permlink: generatePermlink(title) + "proposal",
+                        max_accepted_payout: '10000.000 HBD',
+                        percent_hbd: 10000,
+                        allow_votes: true,
+                        allow_curation_rewards: true,
+                        extensions: [
+                            [0, {
+                                beneficiaries: [{
+                                    account: 'steemskate',
+                                    weight: 5000
+                                }]
+                            }]
+                        ]
+                    })
+                }
+            };
+
+            const response = await commentWithKeychain(hivePostMetadata);
+            if (response?.success) {
+                const receipt = await createProposal(web3, proposalData);
+                setState({ currentBlockNumber, confetti: true });
+
+            } else {
+                throw new Error('Hive post unsuccessful');
+            }
         } catch (error: any) {
             console.error("Failed to create proposal:", error);
             alert("Error creating proposal: " + error.message);
         }
-    }
+    }, [web3, proposalBody, connectedUserAddress, title, hiveUser]);
 
     return (
         <>
-            {confetti && <Confetti />}
-
+            {state.confetti && <Confetti />}
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
@@ -102,7 +135,7 @@ const CreateProposalConfirmationModal: React.FC<CreateProposalConfirmationModalP
                         </ReactMarkdown>
                     </ModalBody>
                     <ModalFooter>
-                        <Button colorScheme="blue" mr={3} onClick={() => handleCreateProposal(web3, proposalBody, connectedUserAddress, title)}>
+                        <Button colorScheme="blue" mr={3} onClick={handleCreateProposal}>
                             Confirm
                         </Button>
                         <Button colorScheme="green" mr={3} onClick={onClose}>
@@ -115,4 +148,4 @@ const CreateProposalConfirmationModal: React.FC<CreateProposalConfirmationModalP
     );
 };
 
-export default CreateProposalConfirmationModal;
+export default React.memo(CreateProposalConfirmationModal);
