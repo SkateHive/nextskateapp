f08965c812767d30ff1d6bf696c40defb36ad5ab
bgrana75 <bgrana@gmail.com>
2024-04-21 11:12:37 -0700

    clean up

--- src/app/profile/[username]/profileDashboard.tsx
+++ src/app/profile/[username]/profileDashboard.tsx
@@ -5,7 +5,7 @@ import { Button } from "@chakra-ui/react";
 const ProfileDashboard = () => {
     const user = useHiveUser();
 
-    const hasVotedForSkatehive = user?.hiveUser?.witness_votes?.includes('skatehive');
+    //const hasVotedForSkatehive = user?.hiveUser?.witness_votes?.includes('skatehive');
 
     const handleVote = () => {
         // Add your transaction logic here
@@ -15,8 +15,8 @@ const ProfileDashboard = () => {
         <div>
             <p>Welcome, {String(user?.hiveUser?.name)}</p>
             <p> Hive Power: {String(user?.hiveUser?.vesting_shares)}</p>
-            <p> Witness: {String(user?.hiveUser?.witness_votes)}</p>
-            {!hasVotedForSkatehive && <Button onClick={handleVote}>Vote for Skatehive</Button>}
+            
+           
         </div>
     )
 }
