7cc8fcf0434a893925ee346410fbad3abbd189b5
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-05-19 15:22:07 -0300

    little love in dao page

--- src/app/dao/components/daoTreasure.tsx
+++ src/app/dao/components/daoTreasure.tsx
@@ -12,7 +12,8 @@ import {
     Center,
     Divider,
     Flex,
-    VStack
+    VStack,
+    HStack
 } from "@chakra-ui/react";
 import axios from 'axios';
 import * as Types from "../../wallet/types";
@@ -21,28 +22,22 @@ import { mainnet } from 'viem/chains';
 import { http } from 'viem';
 import { useBalance } from 'wagmi';
 
-// wagmi config on eth
-export const wagmiConfig = getDefaultConfig({
-    appName: "SkateHive",
-    projectId: "52f3a9b032f5caf26719af6939715629",
-    chains: [mainnet],
-    transports: {
-        [mainnet.id]: http(),
-    },
-})
 
 const HOT_ADDRESS = '0xB4964e1ecA55Db36a94e8aeFfBFBAb48529a2f6c';
 const MULTISIG_ADDRESS = '0x5501838d869B125EFd90daCf45cDFAC4ea192c12' as `0x${string}`;
 const DaoTreasure = () => {
 
-    const multisigBalance = useBalance({ address: MULTISIG_ADDRESS });
+    const multisigBalance = useBalance({
+        address: MULTISIG_ADDRESS,
+        chainId: mainnet.id,
+    });
+    console.log(multisigBalance)
     const balance = useBalance({ address: MULTISIG_ADDRESS });
     const [hotWalletbalance, setWalletbalance] = useState("0")
 
     useEffect(() => {
         const fetchData = async () => {
             const Portfolio = await axios.get(`https://pioneers.dev/api/v1/portfolio/${HOT_ADDRESS}`);
-            console.log(Portfolio.data.totalNetWorth);
             setWalletbalance(Portfolio.data.totalNetWorth);
         };
 
@@ -52,8 +47,7 @@ const DaoTreasure = () => {
 
     return (
         <Card
-            bg="black"
-            border={"1px solid yellow"}>
+            bg="black">
             <Center>
 
                 <CardHeader>DAO Treasure</CardHeader>
@@ -64,23 +58,24 @@ const DaoTreasure = () => {
                 display="flex"
                 flexDirection="column"
                 alignItems="center"
-
             >
-                <VStack >
-                    <Text color={"white"}>skatehive.eth</Text>
-                    <Text>{Number(hotWalletbalance).toFixed(2)} USD</Text>
-                </VStack>
-                <VStack >
+                <Flex flexDirection={{ base: 'column', md: 'row' }} gap={10}>
+                    <VStack >
+                        <Text color={"white"}>skatehive.eth</Text>
+                        <Text fontSize={"38px"}>{Number(hotWalletbalance).toFixed(2)} USD</Text>
+                    </VStack>
+                    <Divider orientation="vertical" />
+                    <VStack >
 
-                    <Text color="white">Multisig Wallet Total Balance USD: </Text>
+                        <Text color="white">ETH Multisig Wallet </Text>
 
-                    <Text>
-                        {Number(multisigBalance)}</Text>
-                </VStack>
+                        <Text fontSize={"38px"}>
+                            {Number(multisigBalance.data?.formatted)} ETH </Text>
+                    </VStack>
+                </Flex>
             </CardBody>
-            <CardFooter>
-            </CardFooter>
-        </Card>
+
+        </Card >
     );
 }
 
