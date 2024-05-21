032f16ee6a60a346839e203306fefdba72138bd1
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-05-21 17:41:36 -0300

    apis

--- src/lib/hive/hiveclient.tsx
+++ src/lib/hive/hiveclient.tsx
@@ -1,6 +1,8 @@
 import { Client } from "@hiveio/dhive"
 
 const HiveClient = new Client([
+  "https://api.openhive.network",
+  "https://api.hive.blog",
   "https://anyx.io",
   "https://api.deathwing.me"
 ])
