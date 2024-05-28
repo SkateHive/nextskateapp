a470bb57340d4875eee5e7fe8713af701f5bad62
Jhonattan ferri <jhoontutors@gmail.com>
2024-05-28 18:14:05 -0300

    fix: cursor cursor fixed up

--- src/app/Cursor/Cursor.tsx
+++ src/app/Cursor/Cursor.tsx
@@ -4,36 +4,55 @@ import { useEffect, useState } from 'react';
 import cursorImage from '../../../public/cursos.png';
 import styles from './Cursor.module.css';
 
+interface Position {
+  x: number;
+  y: number;
+}
+
 const Cursor = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
 
   useEffect(() => {
-    const handleMouseMove = (e: { clientX: any; clientY: any; }) => {
+    const handleMouseMove = (e: MouseEvent) => {
       setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    const handleMouseOver = (e: MouseEvent) => {
+      if ((e.target as HTMLElement).closest('a, button')) {
+        setIsHovering(true);
+      } else {
+        setIsHovering(false);
+      }
     };
 
     document.body.style.cursor = 'none';
 
     window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseover', handleMouseOver);
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseover', handleMouseOver);
       document.body.style.cursor = 'auto';
     };
   }, []);
 
   return (
-    <div>
+    <>
       <style jsx global>{`
         body, * {
           cursor: none !important;
         }
       `}</style>
       
-      <div className={styles.cursor} style={{ left: position.x, top: position.y }}>
-        <Image src={cursorImage} alt="Custom Cursor" />
+      <div 
+        className={`${styles.cursor} ${isHovering ? styles.hover : ''}`} 
+        style={{ left: `${position.x - 12}px`, top: `${position.y - 10}px`, position: 'absolute' }}
+      >
+        <Image src={cursorImage} alt="Custom Cursor" width={40} />
       </div>
-    </div>
+    </>
   );
 };
 
