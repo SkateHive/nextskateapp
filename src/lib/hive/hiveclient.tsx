92b3177ef0fc0250fab60a7123105a4c50c99531
Vlad Nikolaev <web3warrior@macbook.local>
2024-06-11 09:52:49 -0300

    hiveclient

--- src/lib/hive/hiveclient.tsx
+++ src/lib/hive/hiveclient.tsx
@@ -1,9 +1,9 @@
 import { Client } from "@hiveio/dhive"
 
 const HiveClient = new Client([
+  "https://api.deathwing.me",
   "https://api.hive.blog",
   "https://anyx.io",
-  "https://api.deathwing.me",
   "https://hive-api.arcange.eu",
   "https://hive-api.3speak.tv",
   "https://techcoderx.com",
