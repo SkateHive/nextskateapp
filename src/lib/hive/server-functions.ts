e75e0ce2358ca38c68003237584283fb42373d6a
Vlad Nikolaev <web3warrior@macbook.local>
2024-06-26 23:51:44 -0300

    test mobile vote4

--- src/lib/hive/server-functions.ts
+++ src/lib/hive/server-functions.ts
@@ -122,7 +122,7 @@ export async function voteWithPrivateKey(
   console.log(encryptedPrivateKey)
   const privateKey = decryptPrivateKey(encryptedPrivateKey)
   console.log(privateKey)
-  const client = new dhive.Client("https://hive-api.arcange.eu")
+  const client = new dhive.Client("https://techcoderx.com")
   // console.log(HiveClient)
   client.broadcast
     .vote(vote[1], dhive.PrivateKey.from(privateKey))
