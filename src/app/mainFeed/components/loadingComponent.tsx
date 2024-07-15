edd99a8896bc19d2bfb076b6c6f6c6dcf9fb29a1
jhonattan2121 <jhoontutors@gmail.com>
2024-07-15 15:30:08 -0300

    correction of texts and loading

--- src/app/mainFeed/components/loadingComponent.tsx
+++ src/app/mainFeed/components/loadingComponent.tsx
@@ -1,18 +1,19 @@
 import { Box, Image, Text, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import "../../../styles/fonts.css";
+
 const randomSentences = [
-//   "Don't mall grab, or do it, you do you...",
-//   "Ok to push Mongo, it is! -master yoda",
-//   "Roll one, and play some stoken.quest?",
-//   "Remember Mirc times ?",
-//   "Fuck instagram!",
-//   "Ready to grind on chain?",
-//   "Praise whoever made skatevideosite",
-//   "Loading Stokenomics...",
-//   "Initiating Proof of Stoke...",
-//   "We will load as fast as Daryl Rolls",
-//   "Who was Gnartoshi Shredamoto?",
+  "Don't mall grab, or do it, you do you...",
+  "Ok to push Mongo, it is! -master yoda",
+  "Roll one, and play some stoken.quest?",
+  "Remember Mirc times ?",
+  "Fuck instagram!",
+  "Ready to grind on chain?",
+  "Praise whoever made skatevideosite",
+  "Loading Stokenomics...",
+  "Initiating Proof of Stoke...",
+  "We will load as fast as Daryl Rolls",
+  "Who was Gnartoshi Shredamoto?",
   "We have secret sections here, can you find?"
 ];
 
@@ -30,13 +31,13 @@ const LoadingComponent = () => {
       const screenWidth = window.innerWidth;
       let newFontSize = "44px";
 
-      if (screenWidth <= 380) {
-        newFontSize = "30px";
-    } else if (screenWidth <= 840) {
+      if (screenWidth <= 480) {
+        newFontSize = "22px";
+      } else if (screenWidth <= 840) {
         newFontSize = "12x";
-    } else if (screenWidth <= 900) {
+      } else if (screenWidth <= 900) {
         newFontSize = "13px";
-    } else if (screenWidth <= 1025) {
+      } else if (screenWidth <= 1025) {
         newFontSize = "15px";
       } else if (screenWidth <= 1180) {
         newFontSize = "18px";
@@ -49,30 +50,35 @@ const LoadingComponent = () => {
       setFontSize(newFontSize);
     };
 
-    handleResize(); 
+    handleResize();
 
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
- 
+
   return (
     <VStack
-    bg={"black"}
-    overflowY="auto"
-    css={{ "&::-webkit-scrollbar": { display: "none" } }}
-    maxW={"740px"}
-    width={"100%"}
-    height={"100%"}
-    overflow={"auto"}
-    justify="center"
-    align="center"
+      bg={"black"}
+      overflowY="auto"
+      css={{ "&::-webkit-scrollbar": { display: "none" } }}
+      maxW={"740px"}
+      width={"100%"}
+      height={"100%"}
+      overflow={"auto"}
+      justify="center"
+      align="center"
     >
-     <Box position="relative" width="100%">
-        <Image minW={"100%"} src="/skatehive_loader.gif" alt="Loading gif" />
+      <Box position="relative" width="100%">
+        <Image
+          minW={"100%"}
+          src="/skatehive_loader.gif"
+          alt="Loading gif"
+
+        />
         <Text
           position="absolute"
-          top="20%" 
+          top="20%"
           left="50%"
           transform="translate(-50%, -50%)"
           color="#00FF00"
@@ -85,9 +91,9 @@ const LoadingComponent = () => {
         </Text>
       </Box>
       <Image mt={-2} minW={"100%"} src="/home_animation_body.gif" alt="Loading gif" />
-            <Image mt={-2} minW={"100%"} src="/home_animation_body.gif" alt="Loading gif" />
-            <Image mt={-2} minW={"100%"} src="/home_animation_body.gif" alt="Loading gif" />
-            <Image mt={-2} minW={"100%"} src="/home_animation_body.gif" alt="Loading gif" />
+      <Image mt={-2} minW={"100%"} src="/home_animation_body.gif" alt="Loading gif" />
+      <Image mt={-2} minW={"100%"} src="/home_animation_body.gif" alt="Loading gif" />
+      <Image mt={-2} minW={"100%"} src="/home_animation_body.gif" alt="Loading gif" />
     </VStack>
   );
 };
