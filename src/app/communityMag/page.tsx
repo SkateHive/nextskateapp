71da9868653d0c82edc41791cbf133a3726dd4d6
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-08-12 22:04:59 -0300

    refactor: update identation and remove unused code

--- src/app/communityMag/page.tsx
+++ src/app/communityMag/page.tsx
@@ -1,28 +1,7 @@
-'use client';
 import FullMag from "@/components/Magazine/test/fullMag";
-import { Box, Button, Flex } from "@chakra-ui/react";
-import React from "react";
 
 const CommunityMagPage = () => {
-    const handleBackClick = () => {
-        if (window) {
-            window.location.href = '/';
-        }
-    };
-
-
-    return (
-        <>
-
-            <FullMag tag={[{ tag: 'hive-173115', limit: 33 }]} query="created" />
-            {/* <Box>
-                <Button m={2} colorScheme="green" variant={'outline'} onClick={handleBackClick}>
-                    Back
-                </Button>
-            </Box> */}
-        </>
-    );
+  return <FullMag tag={[{ tag: "hive-173115", limit: 33 }]} query="created" />;
 };
 
 export default CommunityMagPage;
-
