ccf6186a1c14646283a9bde477a7a7d3d9c3a19b
sktbrd <sktbrd.eth@gmail.com>
2024-08-18 22:15:11 -0300

    feature: MagazineFullScreenMode/ fix skater profile

--- src/components/Skater/SkaterTabs.tsx
+++ src/components/Skater/SkaterTabs.tsx
@@ -3,6 +3,7 @@ import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import Zine from "../Magazine/test/page";
 import ProfilePosts from "../Profile/ProfilePosts";
 import VideoParts from "../Profile/profileVideos";
+import { QueryProvider } from "@/contexts/QueryContext";
 
 interface ProfilePageProps {
   user: HiveAccount;
@@ -10,39 +11,33 @@ interface ProfilePageProps {
 
 export default function SkaterTabs({ user }: ProfilePageProps) {
   return (
-    <Box>
-      <Tabs isLazy isFitted variant="enclosed-colored">
-        <TabList color={"white"} mb="1em">
-          {/* <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>DashBoard</Tab> */}
-          <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>
-            Zine
-          </Tab>
-          <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>
-            Posts
-          </Tab>
-          <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>
-            VideoParts
-          </Tab>
-          {/* <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>Comments</Tab> */}
-        </TabList>
-        <TabPanels>
-          {/* <TabPanel>
-                        <ProfileDashboard user={user} />
-                    </TabPanel> */}
-          {/* <TabPanel>
-                        <ProfileBlog user={user} />
-                    </TabPanel> */}
-          <TabPanel>
-            <Zine tag={[{ tag: user.name, limit: 20 }]} query="blog" />
-          </TabPanel>
-          <TabPanel>
-            <ProfilePosts user={user} />
-          </TabPanel>
-          <TabPanel>
-            <VideoParts skater={user} />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Box>
+    <QueryProvider query="blog" tag={[{ tag: user.name, limit: 20 }]}>
+      <Box>
+        <Tabs isLazy isFitted variant="enclosed-colored">
+          <TabList color={"white"} mb="1em">
+            <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>
+              Zine
+            </Tab>
+            <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>
+              Posts
+            </Tab>
+            <Tab bg={"black"} _selected={{ bg: "limegreen", color: "black" }}>
+              VideoParts
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Zine tag={[{ tag: user.name, limit: 20 }]} query="blog" />
+            </TabPanel>
+            <TabPanel>
+              <ProfilePosts user={user} />
+            </TabPanel>
+            <TabPanel>
+              <VideoParts skater={user} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </QueryProvider>
   );
 }
