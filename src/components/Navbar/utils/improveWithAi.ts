af9525fee5dc2e41af234ab45ecacc8a315c8032
sktbrd <sktbrd.eth@gmail.com>
2024-08-22 13:51:23 -0300

    ave money with ai

--- src/components/Navbar/utils/improveWithAi.ts
+++ src/components/Navbar/utils/improveWithAi.ts
@@ -10,7 +10,7 @@ const improveWithAi = async (body: string): Promise<string> => {
     const prompt = `Write this better, not too formal though, feel free to use skateboarding slangs but careful not to sound corny: ${body}`;
     const response = await openai.chat.completions.create({
         messages: [{ role: 'user', content: prompt }],
-        model: 'gpt-4',
+        model: 'gpt-4o-mini	',
     });
     const summary = response.choices[0]?.message?.content || 'Our devs forgot to pay the AI bill';
     return summary;
