c4c896a7c38a38785835205f5d6e5b67ea681df6
jhonattan2121 <devferri22@gmail.com>
2024-09-03 13:11:13 -0300

    some changes and corrections to the map, still undergoing testing of the coordinates

---
+++ src/app/map/PostList.tsx
@@ -0,0 +1,86 @@
+import CommentItem from '@/app/mainFeed/components/CommentItem';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+
+export interface PostListProps {
+  comments: any[];
+  visiblePosts: number;
+  username?: string;
+  parentPermlink: string;
+  handleVote: (author: string, permlink: string) => void;
+}
+
+const PostList: React.FC<PostListProps> = ({
+  comments,
+  visiblePosts,
+  parentPermlink,
+  username,
+  handleVote,
+}) => {
+  const [loadedPosts, setLoadedPosts] = useState<any[]>([]);
+  const [loadedCount, setLoadedCount] = useState<number>(5);
+  const [expandedComments, setExpandedComments] = useState<{ [key: string]: boolean }>({});
+
+  useEffect(() => {
+    setLoadedCount(5);
+  }, [parentPermlink]);
+
+  useEffect(() => {
+    const filteredComments = comments
+      .filter(comment => comment.parent_permlink === parentPermlink)
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+      .slice(0, loadedCount);
+
+    setLoadedPosts(filteredComments);
+  }, [comments, loadedCount, parentPermlink]);
+
+  const loadMorePosts = () => {
+    setLoadedCount(loadedCount + visiblePosts);
+  };
+
+  const toggleCommentVisibility = (commentId: string) => {
+    setExpandedComments(prevState => ({
+      ...prevState,
+      [commentId]: !prevState[commentId]
+    }));
+  };
+
+  return (
+    <Box width="full">
+      {loadedPosts.map((comment) => (
+        <Box key={comment.id}>
+          <CommentItem
+            comment={comment}
+            username={username || ""}
+            handleVote={handleVote}
+            onClick={() => toggleCommentVisibility(comment.id)}
+          />
+          {expandedComments[comment.id] && (
+            <Box ml={4} mt={2} pl={4} borderLeft="1px solid gray">
+              {comments.filter(childComment => childComment.parent_permlink === comment.permlink).map(childComment => (
+                <CommentItem
+                  key={childComment.id}
+                  comment={childComment}
+                  username={username || ""}
+                  handleVote={handleVote}
+                />
+              ))}
+            </Box>
+          )}
+        </Box>
+      ))}
+      {comments.length === 0 && (
+        <Flex justify="center" py={4}>
+          <Text>No comments found.</Text>
+        </Flex>
+      )}
+      {comments.length > loadedPosts.length && (
+        <Flex justify="center" py={4}>
+          <Button onClick={loadMorePosts}>Load more posts</Button>
+        </Flex>
+      )}
+    </Box>
+  );
+};
+
+export default PostList;
