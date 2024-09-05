c2307caca497b5d7fdf09d1aed3138a0113f7fb5
jhonattan2121 <devferri22@gmail.com>
2024-09-05 19:19:20 -0300

    corrections to the hive wallet and changes to the carousel feed

--- src/app/mainFeed/components/CommentItem/CustomRightArrow.tsx
+++ src/app/mainFeed/components/CommentItem/CustomRightArrow.tsx
@@ -1,33 +1,37 @@
+import { Box, Icon } from "@chakra-ui/react";
 import { LuArrowRightSquare } from "react-icons/lu";
 
 interface CustomRightArrowProps {
-    onClick: () => void;
+    onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+    color?: string;
 }
 
-const CustomRightArrow = ({ onClick }: CustomRightArrowProps) => {
+const CustomRightArrow = ({ onClick, color }: CustomRightArrowProps) => {
+    const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+        onClick(e);
+    };
     return (
-        <div
-            onClick={onClick}
-            style={{
-                position: 'absolute',
-                right: '0',
-                zIndex: '10',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: '40px',
-                height: '100%',
-                cursor: 'pointer',
-                opacity: 0,
-                transition: 'opacity 0.3s ease',
-                backdropFilter: 'blur(3px)',
-                borderTopRightRadius: '10px',
-                borderBottomRightRadius: '10px',
-            }}
-            className="custom-arrow"
+        <Box
+            onClick={handleClick}
+            position="absolute"
+            right="0"
+            zIndex="10"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            width={{ base: '30px', md: '40px' }}
+            height="100%"
+            cursor="pointer"
+            opacity={0}
+            transition="opacity 0.3s ease"
+            backdropFilter="blur(3px)"
+            borderTopRightRadius="10px"
+            borderBottomRightRadius="10px"
+            _hover={{ opacity: 1 }}
         >
-            <LuArrowRightSquare color="white" size="20px" />
-        </div>
+            <Icon as={LuArrowRightSquare} color={color} boxSize={{ base: '30px', md: '40px', lg: '50px' }} />
+        </Box>
     );
 };
 
