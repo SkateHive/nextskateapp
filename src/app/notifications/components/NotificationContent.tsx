c59cbb6f5aff3c762cc26f5cd4b27905223c3433
sktbrd <sktbrd.eth@gmail.com>
2024-09-11 00:32:48 -0300

    Notifications Revamping !

--- src/app/notifications/components/NotificationContent.tsx
+++ src/app/notifications/components/NotificationContent.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
 import { calculateTimeAgo } from "@/lib/utils"
 import {
   Avatar,
-  Badge,
   Flex,
   HStack,
   IconButton,
@@ -11,6 +10,8 @@ import {
   Text,
   Tooltip,
   Button,
+  Image,
+  Box,
 } from "@chakra-ui/react"
 import { ExternalLink } from "lucide-react"
 import { Notification } from "../page"
@@ -19,40 +20,46 @@ import { checkFollow, changeFollow } from "@/lib/hive/client-functions"
 import { Comment } from "@hiveio/dhive"
 import ReactMarkdown from "react-markdown"
 import { MarkdownRenderers } from "@/app/upload/utils/MarkdownRenderers"
+import ReplyModal from "@/app/mainFeed/components/replyModal"
+
 interface NotificationContentProps {
   notification: Notification
   username: string
 }
 
-function getTypeColor(type: string) {
-  switch (type) {
-    case "mention":
-      return "yellow"
-    case "vote":
-      return "green"
-    case "reply":
-    case "reply_comment":
-      return "blue"
-    case "follow":
-      return "purple"
-    default:
-      return "gray"
-  }
+interface JsonMetadata {
+  image?: string[];
+  [key: string]: any;
 }
 
+// Create an in-memory cache
+const postCache: Record<string, Comment | null> = {}
 // Helper function to extract permlink from notification URL
 const extractPermlink = (url: string) => {
   const segments = url.split("/")
   return segments[segments.length - 1] // Get the last part of the URL
 }
-
 export function NotificationContent({
   notification,
   username,
 }: NotificationContentProps) {
-  const [post, setPost] = useState<Comment | null>(null)
-  const [isFollowing, setIsFollowing] = useState<boolean | null>(null)
-  const [loadingFollow, setLoadingFollow] = useState(false)
+  const [post, setPost] = useState<Comment | null>(null);
+  const [parentPost, setParentPost] = useState<Comment | null>(null); // Store parent post details
+  const [isFollowing, setIsFollowing] = useState<boolean | null>(null);
+  const [loadingFollow, setLoadingFollow] = useState(false);
+  const [isReplyModalOpen, setIsReplyModalOpen] = useState(false);
+
+  // Memoized function to get post from cache or fetch from API
+  const getCachedPostDetails = async (author: string, permlink: string) => {
+    const cacheKey = `${author}/${permlink}`;
+    if (postCache[cacheKey]) {
+      return postCache[cacheKey];
+    } else {
+      const postDetails = await getPostDetails(author, permlink);
+      postCache[cacheKey] = postDetails;
+      return postDetails;
+    }
+  }
 
   // Load post details automatically for reply and reply_comment types
   useEffect(() => {
@@ -60,13 +67,18 @@ export function NotificationContent({
       if (notification.type === "reply" || notification.type === "reply_comment") {
         const permlink = extractPermlink(notification.url)
         try {
-          const postDetails = await getPostDetails(String(notification.user), permlink)
+          const postDetails = await getCachedPostDetails(String(notification.user), permlink)
           setPost(postDetails)
+
+          // Fetch the parent post and cache it
+          const parentPostDetails = await getCachedPostDetails(postDetails.parent_author, postDetails.parent_permlink)
+          setParentPost(parentPostDetails) // Set the parent post
         } catch (error) {
           console.error("Error fetching post details", error)
         }
       } else {
-        setPost(null) // Clear post when switching tabs to non-reply types
+        setPost(null); // Clear post when switching tabs to non-reply types
+        setParentPost(null); // Clear parent post
       }
     }
     fetchPostDetails()
@@ -76,7 +88,7 @@ export function NotificationContent({
   useEffect(() => {
     if (notification.type === "follow") {
       const fetchFollowStatus = async () => {
-        const status = await checkFollow(username, String(notification.user)) // Replace "currentUser" with the current logged-in user
+        const status = await checkFollow(username, String(notification.user))
         setIsFollowing(status)
       }
       fetchFollowStatus()
@@ -86,48 +98,101 @@ export function NotificationContent({
   // Handle follow/unfollow action
   const handleFollowToggle = async () => {
     setLoadingFollow(true)
-    await changeFollow(username, String(notification.user)) // Replace "currentUser" with the current logged-in user
+    await changeFollow(username, String(notification.user))
     const updatedStatus = await checkFollow(username, String(notification.user))
     setIsFollowing(updatedStatus)
     setLoadingFollow(false)
   }
 
+  // Safely parse the JSON metadata of the parent post
+  const parentMetadata: JsonMetadata | null = useMemo(() => {
+    if (!parentPost?.json_metadata) {
+      return null;
+    }
+
+    if (typeof parentPost.json_metadata === "string") {
+      try {
+        return JSON.parse(parentPost.json_metadata) as JsonMetadata;
+      } catch (error) {
+        console.error("Error parsing parent post json_metadata", error);
+        return null;
+      }
+    }
+
+    return parentPost.json_metadata as JsonMetadata;
+  }, [parentPost?.json_metadata]);
+
   return (
     <Flex
       align={"center"}
-      px={2}
-      py={4}
-      _hover={{ border: "1px solid", borderColor: "limegreen" }}
+      px={4}
+      py={3}
+      _hover={{ border: "1px solid", borderColor: "gray.600" }}
       gap={4}
       color={"white"}
     >
-      <Link href={`/skater/${notification.user}`}>
-        <Avatar
-          boxSize={"70px"}
-          name={notification.user}
-          src={`https://images.ecency.com/webp/u/${notification.user}/avatar/small`}
-          borderRadius={"100%"}
+      {isReplyModalOpen && (
+        <ReplyModal
+          isOpen={isReplyModalOpen}
+          onClose={() => setIsReplyModalOpen(false)}
+          comment={post}
+          onNewComment={() => { }}
         />
-      </Link>
+      )}
       <Stack flexGrow={1} gap={1}>
-        <HStack>
-
-          <Text fontSize={"22px"}>
-            <Link fontWeight={"bold"} href={`/skater/${notification.user}`}>
-              @{notification.user}
-            </Link>{" "}
-            {notification.msg}
-          </Text>
-          <Text fontSize="18px" color="darkgray" fontWeight="400">
-            {calculateTimeAgo(notification.date)} ago
-          </Text>
+        <HStack align={"center"} justify={"space-between"} w={"full"}>
+          <Flex align={"center"} gap={2}>
+            <Link href={`/skater/${notification.user}`}>
+              <Avatar
+                boxSize={"45px"}
+                name={notification.user}
+                src={`https://images.ecency.com/webp/u/${notification.user}/avatar/small` || '/pepenation.gif'}
+                borderRadius={"full"}
+              />
+            </Link>
+            <Box>
+              <Text fontSize={"18px"} fontWeight={"bold"}>
+                <Link color={"yellow.400"} href={`/skater/${notification.user}`}>
+                  @{notification.user}
+                </Link>{" "}
+                {notification.msg}
+              </Text>
+              <Text fontSize="14px" color="gray.500" fontWeight="400">
+                {calculateTimeAgo(notification.date)} ago
+              </Text>
+            </Box>
+          </Flex>
+          {/* Render the small thumbnail image */}
+          {parentMetadata?.image && (
+            <Box w={"50px"} h={"50px"}>
+              <Image
+                src={parentMetadata.image[0]}
+                alt={parentPost?.title || "Post thumbnail"}
+                borderRadius={"10px"}
+                objectFit={"cover"}
+                w={"full"}
+                h={"full"}
+              />
+            </Box>
+          )}
         </HStack>
 
         {/* Only show post content for reply and reply_comment */}
         {post !== null && (notification.type === "reply" || notification.type === "reply_comment") && (
-          <ReactMarkdown
-            components={MarkdownRenderers}
-          >{post.body}</ReactMarkdown>
+          <Box mt={2}>
+            <ReactMarkdown components={MarkdownRenderers}>{post.body}</ReactMarkdown>
+
+            <Flex justifyContent={"flex-start"} mt={2}>
+              <Button
+                size="sm"
+                colorScheme="green"
+                onClick={() => setIsReplyModalOpen(true)}
+                variant={"ghost"}
+              >
+                Reply
+              </Button>
+            </Flex>
+          </Box>
         )}
       </Stack>
 
