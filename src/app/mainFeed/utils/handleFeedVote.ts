9c322d012de3e2fe6c7b4e2853bf63863e23b973
sktbrd <sktbrd.eth@gmail.com>
2024-09-30 00:14:33 -0300

    voe button slider1

--- src/app/mainFeed/utils/handleFeedVote.ts
+++ src/app/mainFeed/utils/handleFeedVote.ts
@@ -2,15 +2,21 @@ import { vote } from "@/lib/hive/client-functions"
 import { voteWithPrivateKey } from "@/lib/hive/server-functions"
 import { VoteOperation } from "@hiveio/dhive"
 // Modify handleVote function to accept weight parameter
-export const handleVote = async (author: string, permlink: string, username: string, weight: number) => {
+// Modify handleVote function to accept weight parameter
+export const handleVote = async (
+  author: string,
+  permlink: string,
+  username: string,
+  weight?: number // Make weight optional
+) => {
   const loginMethod = localStorage.getItem("LoginMethod");
   if (!username) {
     console.error("Username is missing");
     return;
   }
 
-  // Normalize the weight to a 0-10000 range
-  const normalizedWeight = Math.min(Math.max(weight, 0), 10000); // Ensure it's between 0 and 10000
+  // Use 1000 as default weight if not specified
+  const normalizedWeight = Math.min(Math.max(weight || 1000, 0), 10000); // Ensure it's between 0 and 10000
 
   if (loginMethod === "keychain") {
     await vote({
@@ -35,4 +41,3 @@ export const handleVote = async (author: string, permlink: string, username: str
     voteWithPrivateKey(encryptedPrivateKey, vote);
   }
 };
-
