c02e43af4a43f3d27fd6f2cde853eb4a3dcff7d1
jhonattan2121 <devferri22@gmail.com>
2024-11-26 12:06:42 -0300

    feat: postcard footer improved and new

--- src/components/PostCard/Footer.tsx
+++ src/components/PostCard/Footer.tsx
@@ -1,20 +1,54 @@
-import { usePostContext } from "@/contexts/PostContext"
-import { useHiveUser } from "@/contexts/UserContext"
-import { CardFooter, Flex } from "@chakra-ui/react"
-import TipButton from "./TipButton"
-import Vote from "./Vote"
+import VotingButton from "@/components/ButtonVoteComponent/VotingButton";
+import { usePostContext } from "@/contexts/PostContext";
+import { useHiveUser } from "@/contexts/UserContext";
+import { HiveAccount } from "@/lib/useHiveAuth";
+import { CardFooter, HStack, Text, Tooltip } from "@chakra-ui/react";
+import { useState } from "react";
 
-export default function Footer() {
-  const { post } = usePostContext()
-  const { hiveUser } = useHiveUser()
+interface FooterProps {
+  username?: string;
+}
+
+export default function Footer({ username }: FooterProps) {
+  const { post } = usePostContext();
+  const [postEarnings, setPostEarnings] = useState(Number(post.getEarnings().toFixed(2)));
+  const [isValueTooltipOpen, setIsValueTooltipOpen] = useState(false);
+  const { hiveUser, voteValue } = useHiveUser();
+
+  const usernameString = username
+    ? typeof username === "string"
+      ? username
+      : (username as HiveAccount)?.name || ""
+    : hiveUser?.name || "";
+
+  const toggleValueTooltip = () => {
+    setIsValueTooltipOpen(true);
+    setTimeout(() => {
+      setIsValueTooltipOpen(false);
+    }, 3000);
+  };
 
   return (
     <CardFooter pt={0} flexDirection={"column"} gap={2} key={hiveUser?.name}>
-      {/* <Flex w={"100%"} justify={"space-between"} align={"center"}> */}
-      {/* <TipButton author={post.author} /> */}
-      <Vote />
+      <HStack justifyContent="space-between" width="100%">
+        <VotingButton comment={post} username={usernameString} toggleValueTooltip={toggleValueTooltip} />
 
-      {/* </Flex> */}
+        <Tooltip
+          label={`+$${voteValue.toFixed(6)}`}
+          placement="top"
+          isOpen={isValueTooltipOpen}
+          hasArrow
+        >
+          <Text
+            fontWeight={"bold"}
+            color={"green.400"}
+            cursor={"pointer"}
+            mt={2}
+          >
+            ${postEarnings.toFixed(2)}
+          </Text>
+        </Tooltip>
+      </HStack>
     </CardFooter>
-  )
+  );
 }
