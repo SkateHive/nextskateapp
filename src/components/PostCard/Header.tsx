cf4ba9f69fbe6d44f7842c4f74a8d5d52cfef773
sktbrd <sktbrd.eth@gmail.com>
2025-02-16 20:55:13 -0300

    feat: refactor Header component to enhance sharing functionality and improve menu structure

--- src/components/PostCard/Header.tsx
+++ src/components/PostCard/Header.tsx
@@ -16,104 +16,92 @@ import {
   useDisclosure,
   useMediaQuery
 } from "@chakra-ui/react";
-import {
-  ChevronDownIcon,
-  Copy,
-  ExternalLink,
-  Eye,
-  Twitter
-} from "lucide-react";
+
 import moment from "moment-timezone";
 import Link from "next/link";
-import { useState } from "react";
-import { FaDiscord } from "react-icons/fa";
+import { useState, ReactNode } from "react";
+import { FaCopy, FaDiscord, FaEye, FaLink, FaTwitter } from "react-icons/fa";
 import AuthorAvatar from "../AuthorAvatar";
 import PostModal from "../PostModal";
 import { EditModal } from "../PostModal/editMagPostModal";
-
-
+import { useRouter } from "next/navigation";
+import { IoIosArrowDown } from "react-icons/io";
 
 type Variant = "preview" | "open";
 interface HeaderInterface {
   variant?: Variant;
 }
 
+interface CustomMenuItemProps {
+  onClick: () => void;
+  icon: React.ReactElement;
+  children: ReactNode;
+}
+
+const CustomMenuItem = ({ onClick, icon, children }: CustomMenuItemProps) => (
+  <MenuItem
+    bg={"black"}
+    icon={icon}
+    onClick={onClick}
+    _hover={{ bg: "gray.700" }}
+  >
+    {children}
+  </MenuItem>
+);
+
 export default function Header({ variant = "preview" }: HeaderInterface) {
   const { post } = usePostContext();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isSmallerThan400] = useMediaQuery("(max-width: 400px)");
+  const router = useRouter();
   const postFullUrl = `${window.location.origin}/post${post.url}`;
 
-  const user = useHiveUser()
+  const user = useHiveUser();
   const [isEditing, setIsEditing] = useState(false);
 
   const handleCopyPostLink = () => {
     try {
-      const postPageUrl = postFullUrl;
-      navigator.clipboard.writeText(postPageUrl);
+      navigator.clipboard.writeText(postFullUrl);
     } catch (error) {
       console.error("Failed to copy the link:", error);
     }
   };
 
-  const handleOpenPostLink = () => {
+  const handleOpenLink = (url: string) => {
     try {
-      window.open(postFullUrl, "_blank", "noreferrer noopener");
+      router.push(url);
     } catch (error) {
       console.error("Failed to open the link:", error);
     }
   };
 
-  const handleOpenPostLinkPeakd = () => {
+  const handleShare = async (platform: string) => {
     try {
-      window.open(`https://peakd.com${post.url}`, "_blank", "noreferrer noopener");
+      const postSummary = await getSummary(post.body);
+      const text = `${postSummary} ${postFullUrl}`;
+      const encodedText = encodeURI(text);
+      let url = "";
+
+      switch (platform) {
+        case "WarpCast":
+          url = `https://warpcast.com/~/compose?text=${encodedText}`;
+          break;
+        case "Twitter":
+          url = `https://twitter.com/intent/tweet?text=${encodedText}`;
+          break;
+        case "Discord":
+          navigator.clipboard.writeText(text);
+          url = "https://discord.com/channels/631777256234156033/631778823716864011";
+          break;
+        default:
+          break;
+      }
+
+      if (url) handleOpenLink(url);
     } catch (error) {
-      console.error("Failed to open the link:", error);
+      console.error(`Failed to share on ${platform}:`, error);
     }
   };
-
-  const handleShareWarpCast = async () => {
-    try {
-      const postSummary = await getSummary(post.body).then((summary) => summary);
-      const warptext = `${postSummary} ${postFullUrl}`;
-      const postPageUrl = encodeURI(warptext);
-      window.open(
-        `https://warpcast.com/~/compose?text=${postPageUrl}`,
-        "_blank", "noreferrer noopener");
-    } catch (error) {
-      console.error("Failed to share in WarpCast:", error);
-    }
-  };
-
-  const handleShareTwitter = async () => {
-    try {
-      const postSummary = await getSummary(post.body).then((summary) => summary);
-      const tweetText = `${postSummary} ${postFullUrl}`;
-      const postPageUrl = encodeURI(tweetText);
-      window.open(
-        `https://twitter.com/intent/tweet?text=${postPageUrl}`,
-        "_blank", "noreferrer noopener");
-    } catch (error) {
-      console.error("Failed to share in Twitter:", error);
-    }
-  };
-
-  const handleShareDiscord = async () => {
-    try {
-      const postSummary = getSummary(post.body).then((summary) => summary);
-      const postPageUrl = encodeURI(postFullUrl);
-      const discordText = `${postSummary} ${postPageUrl}`;
-      navigator.clipboard.writeText(discordText);
-      window.open(
-        "https://discord.com/channels/631777256234156033/631778823716864011",
-        "_blank", "noreferrer noopener"
-      );
-    } catch (error) {
-      console.error("Failed to share in Discord:", error);
-    }
-  };
-
-
 
   return (
     <>
@@ -126,12 +114,9 @@ export default function Header({ variant = "preview" }: HeaderInterface) {
       )}
       {isOpen && <PostModal isOpen={isOpen} onClose={onClose} />}
       <CardHeader p={2} pb={0}>
-        <Flex
-          gap="4"
-          align={"center"}
-        >
+        <Flex gap="4" align={"center"}>
           <Flex flex="1" gap="2" alignItems="center">
-            <Link href={`/skater/${post.author}`} >
+            <Link href={`/skater/${post.author}`}>
               <AuthorAvatar username={post.author} boxSize={10} borderRadius={20} quality="small" />
             </Link>
             <Flex flexDir="column" gap={1}>
@@ -156,7 +141,6 @@ export default function Header({ variant = "preview" }: HeaderInterface) {
                   }
                 </Text>
                 <Spacer />
-
               </Flex>
               <Text
                 cursor={"pointer"}
@@ -169,78 +153,71 @@ export default function Header({ variant = "preview" }: HeaderInterface) {
               </Text>
             </Flex>
           </Flex>
-
-          <>
-
-
-            <Menu
-              placement={
-                variant == "open" && isSmallerThan400 ? "bottom" : "bottom-end"
-              }
-              colorScheme="white"
+          <Menu
+            placement={
+              variant == "open" && isSmallerThan400 ? "bottom" : "bottom-end"
+            }
+            colorScheme="white"
+          >
+            <MenuButton
+              colorScheme="green"
+              as={Button}
+              variant={"ghost"}
+              width={variant == "open" && isSmallerThan400 ? "100%" : "auto"}
+              minW={0}
+              p={1}
+              h={6}
             >
-
-              <MenuButton
-                colorScheme="green"
-                as={Button}
-                variant={"ghost"}
-                width={variant == "open" && isSmallerThan400 ? "100%" : "auto"}
-                minW={0}
-                p={1}
-                h={6}
+              <IoIosArrowDown />
+            </MenuButton>
+            <MenuList bg={"black"} fontSize={"sm"} color={"white"}>
+              {user.hiveUser?.name === post.author && (
+                <CustomMenuItem
+                  icon={<FaEye size={18} />}
+                  onClick={() => setIsEditing(true)}
+                >
+                  Edit
+                </CustomMenuItem>
+              )}
+              <CustomMenuItem
+                icon={<FaCopy size={18} />}
+                onClick={handleCopyPostLink}
               >
-                <ChevronDownIcon />
-              </MenuButton>
-              <MenuList bg={"black"} fontSize={"sm"}>
-                {user.hiveUser?.name === post.author && (
-                  <MenuItem
-                    bg={"black"}
-                    icon={<Eye size={18} />}
-                    onClick={() => setIsEditing(true)}
-                  >
-                    Edit
-                  </MenuItem>
-                )}
-                <MenuItem bg={"black"}
-                  onClick={handleCopyPostLink} icon={<Copy size={18} />}>
-                  Copy link
-                </MenuItem>
-                <MenuItem bg={"black"}
-                  onClick={handleShareWarpCast}
-                  icon={<ExternalLink size={18} />}
-                >
-                  WarpCast
-                </MenuItem>
-                <MenuItem bg={"black"}
-                  onClick={handleShareTwitter}
-                  icon={<Twitter size={18} />}
-                >
-                  Twitter
-                </MenuItem>
-                <MenuItem bg={"black"}
-                  onClick={handleShareDiscord}
-                  icon={<FaDiscord size={18} />}
-                >
-                  Discord
-                </MenuItem>
-                <MenuDivider />
-                <MenuItem bg={"black"}
-                  onClick={handleOpenPostLink}
-                  icon={<ExternalLink size={18} />}
-                >
-                  Post page
-                </MenuItem>
-                <MenuItem bg={"black"}
-                  onClick={handleOpenPostLinkPeakd}
-                  icon={<ExternalLink size={18} />}
-                >
-                  PeakD
-                </MenuItem>
-              </MenuList>
-            </Menu>
-
-          </>
-
+                Copy link
+              </CustomMenuItem>
+              <CustomMenuItem
+                icon={<FaLink size={18} />}
+                onClick={() => handleShare("WarpCast")}
+              >
+                WarpCast
+              </CustomMenuItem>
+              <CustomMenuItem
+                icon={<FaTwitter size={18} />}
+                onClick={() => handleShare("Twitter")}
+              >
+                Twitter
+              </CustomMenuItem>
+              <CustomMenuItem
+                icon={<FaDiscord size={18} />}
+                onClick={() => handleShare("Discord")}
+              >
+                Discord
+              </CustomMenuItem>
+              <MenuDivider />
+              <CustomMenuItem
+                icon={<FaLink size={18} />}
+                onClick={() => handleOpenLink(postFullUrl)}
+              >
+                Post page
+              </CustomMenuItem>
+              <CustomMenuItem
+                icon={<FaLink size={18} />}
+                onClick={() => handleOpenLink(`https://peakd.com${post.url}`)}
+              >
+                PeakD
+              </CustomMenuItem>
+            </MenuList>
+          </Menu>
         </Flex>
       </CardHeader>
     </>
